6679f056f9bf1c30a9938e04e4ba1c5543efa799
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2023-09-17 14:29:17 +0300

    change package.json

--- app.js
+++ app.js
@@ -19,11 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(logger(formatsLogger));
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
