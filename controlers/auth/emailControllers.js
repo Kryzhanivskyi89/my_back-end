04df2299451c31678ee560f0b2cde2aff0387979
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2023-11-24 11:36:16 +0200

    clear drafts

--- controlers/auth/emailControllers.js
+++ controlers/auth/emailControllers.js
@@ -25,30 +25,8 @@ const resendSubscribe = async (req, res) => {
     message: "Subscription email sent",
   });
 };
-// const subscribe = async (req, res) => {
-//   const { email } = req.body;
-//   const user = await User.findById(req.user._id);
-//   if (!user) {
-//     throw HttpError(401, "Email not found");
-//   }
-//   if (user.subscription) {
-//     res.status(400).json({ message: "Subscription has already been passed" });
-//   }
-
-//   const SubscriptionEm = {
-//     to: email,
-//     subject: "So Yummy - Підписка",
-//     html: `<a target="_blank" href="${FRONT_URL}/subscribe/${user.subscriptionToken}">Click to Subscription email</a>`,
-//   };
-
-//   await sendEmail(SubscriptionEm);
-
-//   res.json({
-//     message: "Subscription email sent",
-//   });
-// };
 
 module.exports = {
   resendSubscribe: ctrlWrapper(resendSubscribe),
-  // subscribe: ctrlWrapper(subscribe),
+  
 };
