92d56b180fb2f2ae942c41345bb4fe183870d6a1
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2024-08-04 09:46:01 +0300

    remove authorization in addOrder controler

--- controlers/orders/addOrder.js
+++ controlers/orders/addOrder.js
@@ -1,13 +1,26 @@
 const Order = require("../../models/orders");
 
 const addOrder = async (req, res) => {
-  const { _id: owner } = req.user;
   try {
-    const addOrder = await Order.create({ ...req.body, owner });
-    res.status(201).json(addOrder);
+    const addOrder = await Order.create(req.body); // Створює нове замовлення тільки з даними із тіла запиту
+    res.status(201).json(addOrder); // Відправляє відповідь із статусом 201 (створено) і повертає створене замовлення
   } catch (error) {
-    res.status(500).json({ message: error.message || "Failed to create order" });
+    res.status(500).json({ message: error.message || "Failed to create order" }); // Відправляє відповідь із статусом 500 (внутрішня помилка сервера) і повідомленням про помилку
   }
 };
 
 module.exports = addOrder;
+
+// const Order = require("../../models/orders");
+
+// const addOrder = async (req, res) => {
+//   const { _id: owner } = req.user;
+//   try {
+//     const addOrder = await Order.create({ ...req.body, owner });
+//     res.status(201).json(addOrder);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message || "Failed to create order" });
+//   }
+// };
+
+// module.exports = addOrder;
