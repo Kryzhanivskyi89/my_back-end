82497dd9decff64a2a8e73beaade2f91c4cc3df3
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2026-03-09 09:52:48 +0200

    change import contacts model

--- services/contacts/contactsService.js
+++ services/contacts/contactsService.js
@@ -1,20 +1,20 @@
-const { Contacts } = require("../../models");
+const Contacts = require("../../models/contacts");
 
 class ContactsService {
     async getAll() {
         const result = await Contacts.find();
         return result;
-    };
+    }
 
     async addContact(contact) {
         const newContact = await Contacts.create({ ...contact });
         return newContact;
-    };
+    }
 
     async deleteContacts(id) {
         const deleteContact = await Contacts.findOneAndRemove({ _id: id });
         return deleteContact;
-    };
+    }
 }
 
 module.exports = new ContactsService();
